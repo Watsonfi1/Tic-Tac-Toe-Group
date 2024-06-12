b2fe2fbc2fa98aa0da619b951daff7b062fcbcdd
Coreywscw <159732880+Coreywscw@users.noreply.github.com>
2024-06-13 09:31:06 +1200

    fr

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 # // Functions \\
+def singleplayer():
+    print("singleplayer")
+def multiplayer():
+    print("multiplayer")
+
 def intro():
+    print("Welcome to Tic-Tac-Toe!")
+    while True:
+            gamemode = input("Would you like to play singleplayer or multiplayer? ").lower()
+            if gamemode == "multiplayer":
+                multiplayer()
+                break
+            elif gamemode == "singleplayer":
+                singleplayer()
+                break
+            else:
+                print("That wasn't an option, try singleplayer or multiplayer.")
+
 
 # // Variables \\
 
 
 # // Main Code \\
+intro()
