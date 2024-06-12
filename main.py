4dfababd1af4f28cd077f5e0fe5514e16b4f32ca
Coreywscw <159732880+Coreywscw@users.noreply.github.com>
2024-06-13 10:18:09 +1200

    Gamemode select and rules

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ def singleplayer():
     print("singleplayer")
 def multiplayer():
     print("multiplayer")
+def gamerules():
+     print("gamerules")
 
 def intro():
     print("Welcome to Tic-Tac-Toe!")
     while True:
-            gamemode = input("Would you like to play singleplayer or multiplayer? ").lower()
-            if gamemode == "multiplayer":
-                multiplayer()
-                break
-            elif gamemode == "singleplayer":
-                singleplayer()
-                break
-            else:
-                print("That wasn't an option, try singleplayer or multiplayer.")
+        gamemode = input("Would you like to play singleplayer or multiplayer? ").lower()
+        if gamemode == "multiplayer":
+            multiplayer()
+            break
+        elif gamemode == "singleplayer":
+            singleplayer()
+            break
+        else:
+            print("That wasn't an option, try singleplayer or multiplayer.")
 
+    gamerule = input("Do you know the rules to Tic-Tac-Toe? ").lower()
+    if gamerule != "yes":
+        gamerules()
+                
+         
 
 # // Variables \\
 
