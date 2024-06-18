8bddeb00fe0e76b28b5f26f5463b68ed911b9bdc
aspdenjwscw <aspdenj@wsc.school.nz>
2024-06-18 14:36:21 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 # // Functions \\
-def WinLoseScore ():
-    global guesses
-    for i in possible_wins:
-        if 
+
 def singleplayer():
     print("singleplayer")
 def multiplayer():
+    global name1
+    global name2
+    global startingplayer
+    gamecomplete = False
+    while True:
+        if startingplayer > 1:
+            while gamecomplete == False:
+                
+            #X starts
+        if startingplayer < 1:
+            #O starts
+
+        #do before ending
+        startingplayer *= -1
+
+def intro():
+    global name1
+    global name2
+    print("Welcome to Tic-Tac-Toe!")
     while True:
         readinstructions = input("Do you want the instructions (Yes/No)? ").lower()
         if readinstructions == "yes":
-             print("\nInstructions: Tic-Tac-Toe is a simple kids game, the aim of the game is to get 3 of your shapes in a row on the 3x3 board. When it is your turn to make a move, you will have to enter the number of the \nsquare you want to place your shape in. You are not able to put your shape in a square that is already taken. Each round, 5 points will be awarded to the winning player.\n\nMultiplayer: If the multiplayer gamemode has been selected, both players will be asked for their name at the start. They will enter how many rounds they want to play. After both names have been selected, each player will be asked if they want to start or not. If both players wish to start, there will be a 50/50 chance, if one player wants to start, they will, and if both players do not want to start\n it will be a 50/50 chance.\n\nSingleplayer: If the single player gamemode has been selected, the player will be playing against a computer generating moves. The player will also enter how many rounds they want to play. They will be \nasked for their name, and then the player will always start.")
+             print("\nInstructions: Tic-Tac-Toe is a simple kids game, the aim of the game is to get 3 of your shapes in a row on the 3x3 board. When it is your turn to make a move, you will have to enter the number of the \nsquare you want to place your shape in. You are not able to put your shape in a square that is already taken. Each round, 5 points will be awarded to the winning player. To make a move, you will enter a number, top left is one, bottom right is nine. Left to right, top to bottom.\n\nMultiplayer: If the multiplayer gamemode has been selected, both players will be asked for their name at the start. They will enter how many rounds they want to play. After both names have been selected, each player will be asked if they want to start or not. If both players wish to start, there will be a 50/50 chance, if one player wants to start, they will, and if both players do not want to start\nit will be a 50/50 chance.\n\nSingleplayer: If the single player gamemode has been selected, the player will be playing against a computer generating moves. The player will also enter how many rounds they want to play. They will be \nasked for their name, and then the player will always start.\n")
              break
         else:
              break
-
-def intro():
-    print("Welcome to Tic-Tac-Toe!")
     while True:
         gamemode = input("Would you like to play singleplayer or multiplayer? ").lower()
         if gamemode == "multiplayer":
@@ -32,7 +45,7 @@ def intro():
 
 # // Variables \\
 guesses = []
-
+startingplayer = 1
 # // Main Code \\
 #Abreviations for Top left, Top Right, ect
 TL = TM = TR = ML = MM = MR = BL = BM = BR = "  "
